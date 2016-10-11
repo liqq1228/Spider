c922b49f37be4b2695283e4a42e3fb8f46948483
曲檀 <qu.tan@outlook.com>
2016-10-12 00:09:52 +0800

    Update douban.py

--- douban/douban.py
+++ douban/douban.py
@@ -7,8 +7,8 @@ import re
 loginUrl = 'https://accounts.douban.com/login'
 formData = {
     "redir": "https://www.douban.com/people/1386103/contacts",
-    "form_email": 'qu.tan@outlook.com',
-    "form_password": '28292469taN',
+    "form_email": '',
+    "form_password": '',
     "login": u'登录'
 }
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'}
